0a2967ff062691c300f57c63a3046e30bb2fda4d
Kevin Lafonta <kev.lafonta@gmail.com>
2023-11-16 08:45:07 +0100

    vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import MyCounter from './components/MyCounter.vue';
+  import MagicBox from './components/MagicBox.vue';
+
+
+  export default{
+    components: {
+      MyCounter,
+      MagicBox,
+    },
+  };
+</script>
+
+<template>
+  <main>
+    <MyCounter/>
+    <MagicBox/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MagicBox.vue
@@ -0,0 +1,26 @@
+<script>
+  export default{
+    data() {
+      return {
+        visible: true,
+        color: 'black'
+      };
+    },
+    methods: {
+	    method1() { this.visible = !this.visible },
+      method2() { this.color = 'red' },
+    }
+  };
+</script>
+
+<template>
+  <div class="box" v-if="visible" @dblclick="method1()" @click="method2()"
+  :style="{ backgroundColor: color }" ></div>
+</template>
+
+<style scoped>
+  .box {
+    width:200px;
+    height:200px;
+  }
+</style>
